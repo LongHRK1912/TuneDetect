89fd34702b850cb185688df0d44bb27267abbfea
longhrk <nguyenduclong05112001@gmail.com>
2025-03-23 21:33:44 +0700

    update base project and add new function

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,23 +1,39 @@
 pluginManagement {
+    includeBuild("build-logic")
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
     repositories {
         google()
         mavenCentral()
     }
 }
-
 rootProject.name = "TuneDetect"
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 include(":app")
+include(":benchmarks")
+include(":core:analytics")
+include(":core:common")
+include(":core:database")
+include(":core:data")
+include(":core:designsystem")
+include(":core:domain")
+include(":core:model")
+include(":core:network")
+include(":core:notifications")
+include(":core:testing")
+include(":core:ui")
+
+include(":lint")
+include(":sync:work")
+include(":sync:sync-test")
+include(":ui-test-hilt-manifest")
+
+include(":feature:tuneDetect")
